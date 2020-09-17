f33d0994d978d156536a4ca02ee5ed8bc721733a
fourside <fourside@gmail.com>
2020-09-17 16:04:45 +0900

    prettier

--- bin/cdk-ec2.ts
+++ bin/cdk-ec2.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { CdkEc2Stack } from '../lib/cdk-ec2-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { CdkEc2Stack } from "../lib/cdk-ec2-stack";
 
 const app = new cdk.App();
-new CdkEc2Stack(app, 'CdkEc2Stack');
+new CdkEc2Stack(app, "CdkEc2Stack");
